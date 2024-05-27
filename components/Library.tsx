365025e1688ef71abed1b6e00cc9d87dfac04bd5
brandaorichard <rih.brandao@gmail.com>
2024-05-27 20:13:09 -0300

    feat: finishes songs fetching & list display

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,10 +3,18 @@
 import useAuthModal from "@/hooks/useAuthModal";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({
+  songs
+}) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -66,7 +74,13 @@ const Library = () => {
         px-3
       "
       >
-        List of Songs
+        {songs.map((item) => (
+          <MediaItem
+            onClick={() => {}}
+            key={item.id}
+            data={item}
+          />
+        ))}
       </div>
     </div>
   )
