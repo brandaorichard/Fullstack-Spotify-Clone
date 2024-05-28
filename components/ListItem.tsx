913b8569471308a401d7b2b0e0a4272406caa0b3
brandaorichard <rih.brandao@gmail.com>
2024-05-27 21:57:59 -0300

    create list item component

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
@@ -15,10 +15,10 @@ const ListItem: React.FC<ListItemProps> = ({
   name,
   href,
 }) => {
-  // const router = useRouter();
+  const router = useRouter();
 
   const onClick = () => {
-    // router.push(href)
+    router.push(href)
   }
 
   return (
